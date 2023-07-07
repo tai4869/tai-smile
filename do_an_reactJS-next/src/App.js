5fc51ba8461532e798be08fd8efff288a5fe0a08
Thanh Tai <tai.btpk@gmail.com>
2023-07-07 22:56:15 +0700

    add phan cuoi

---
+++ do_an_reactJS-next/src/App.js
@@ -0,0 +1,55 @@
+
+import './App.css';
+import Header from "./common/header/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Pages from './pages/Pages';
+import Data from "./components/flashDeals/Data"
+import Sdata from "./components/shop/Sdata"
+import { useState } from 'react';
+import Cart from "./common/Cart/Cart";
+import Footer from './common/footer/Footer';
+
+
+function App() {
+
+  const { productItems } = Data
+  const { shopItems } = Sdata
+  const [CartItem, setCartItem] = useState([])
+
+  const addToCard = (product) => {
+    const productExit = CartItem.find((item) => item.id === product.id)
+    if (productExit) {
+      setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty + 1 } : item)))
+    } else {
+      setCartItem([...CartItem, { ...product, qty: 1 }])
+    }
+  }
+  const decreaseQty = (product) => {
+    const productExit = CartItem.find((item) => item.id === product.id)
+    if (productExit.qty === 1) {
+      setCartItem(CartItem.filter((item) => item.id !== product.id))
+    } else {
+      setCartItem(CartItem.map((item) => (item.id === product.id ? { ...productExit, qty: productExit.qty - 1 } : item)))
+    }
+  }
+  return (
+
+    <>
+      <Router>
+        <Header CartItem={CartItem} />
+        <Switch>
+          <Route path='/' exact>
+            <Pages productItems={productItems} addToCart={addToCard} shopItems={shopItems} />
+          </Route>
+          <Route path='/cart' exact>
+            <Cart CartItem={CartItem} addToCart={addToCard} decreaseQty={decreaseQty} />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
---
+++ do_an_reactJS-next/src/components/mainpage/Sdata.js
@@ -0,0 +1,28 @@
+const Sdata = [
+    { 
+      id: 1,
+      title: "50% Sale Off KHI MUA TẠI THỜI ĐIỂM NÀY",
+      desc: "NƯỚC HOA NÀY THƠM LẮM NÊN MUA ĐANG CÓ DEAL HOT.",
+      cover: './images/SlideCard/slide-1.png',
+    },
+    {
+      id: 2,
+      title: "50% Sale Off KHI MUA TẠI THỜI ĐIỂM NÀY",
+      desc: "NƯỚC HOA NÀY THƠM LẮM NÊN MUA ĐANG CÓ DEAL HOT.",
+      cover: "./images/SlideCard/slide-2.png",
+    },
+    {
+      id: 3,
+      title: "50% Sale Off KHI MUA TẠI THỜI ĐIỂM NÀY",
+      desc: "NƯỚC HOA NÀY THƠM LẮM NÊN MUA ĐANG CÓ DEAL HOT.",
+      cover: "./images/SlideCard/slide-3.png",
+    },
+    {
+      id: 4,
+      title: "50% Sale Off KHI MUA TẠI THỜI ĐIỂM NÀY",
+      desc: "NƯỚC HOA NÀY THƠM LẮM NÊN MUA ĐANG CÓ DEAL HOT.",
+      cover: "./images/SlideCard/slide-4.png",
+    },
+  ]
+  export default Sdata
+  
